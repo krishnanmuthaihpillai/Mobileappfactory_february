83e8463eeaa92f4a1e3531714ffb0fd5513c0c08
krishnan <krishnapillai87@gmail.com>
2016-02-24 11:58:43 +0530

    SaveContact_in_DB_module_added

---
+++ save_contact_in_db/src/test/java/in/mobileappfactory/save_contact_in_db/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package in.mobileappfactory.save_contact_in_db;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
